3448c5def83cca3a0893d8f459d47b18b72112a9
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-02-26 02:20:42 +0900

    iOSのバージョン指定

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import NeumorphismUI
 
 struct ContentView: View {
     var body: some View {
--- Sources/Color+.swift
+++ Sources/Color+.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+@available(iOS 13.0, *)
 @available(OSX 10.15, *)
 public extension Color {
     init(hex: String, opacity: Double = 1.0) {
