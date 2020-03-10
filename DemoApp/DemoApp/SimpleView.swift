e309735f1fffec125a5912e53b38a6b7d97aea52
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-03-11 06:07:26 +0900

    Add SimpleView

---
+++ DemoApp/DemoApp/SimpleView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import NeumorphismUI
+
+struct SimpleView: View {
+    @EnvironmentObject var neumorphism: NeumorphismManager
+
+    var body: some View {
+        ZStack {
+            neumorphism.color.edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(neumorphism.color)
+                .frame(width: 200, height: 200)
+                .neumorphismShadow()
+        }
+    }
+}
+
+struct SimpleView_Previews: PreviewProvider {
+    static let neumorphism = NeumorphismManager(
+        isDark: false,
+        lightColor: Color(hex: "C1D2EB"),
+        darkColor: Color(hex: "2C292C")
+    )
+    static var previews: some View {
+        SimpleView()
+            .environmentObject(neumorphism)
+    }
+}
