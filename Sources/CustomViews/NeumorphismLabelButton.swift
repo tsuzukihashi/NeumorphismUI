efffec6129d5f0de876730599a06e8f2d7685d38
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-03-13 14:32:25 +0900

    Add NeumorphismLabelButton

---
+++ Sources/CustomViews/NeumorphismLabelButton.swift
@@ -0,0 +1,70 @@
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct NeumorphismLabelButton: View {
+    @EnvironmentObject var neumorphism: NeumorphismManager
+    @State var isSelected = false
+    private var text: String
+    private var font: Font?
+    private var color: Color?
+    private var width: CGFloat
+    private var height: CGFloat
+    private var cornerRadius: CGFloat
+    private var handler: (() -> Void)?
+    
+    public init
+        (
+        text: String = "Button",
+        font: Font? = nil,
+        color: Color? = nil,
+        width: CGFloat = 100,
+        height: CGFloat = 100,
+        cornerRadius: CGFloat = 10,
+        handler: (() -> Void)? = nil
+    ) {
+        self.text = text
+        self.font = font
+        self.color = color
+        self.width = width
+        self.height = height
+        self.cornerRadius = cornerRadius
+        self.handler = handler
+    }
+    
+    public var body: some View {
+        HighlightableButton(action: {
+            self.isSelected.toggle()
+            self.handler?()
+        }) { isHeighlight in
+            ZStack {
+                RoundedRectangle(cornerRadius: self.cornerRadius)
+                    .fill(self.neumorphism.color)
+                    .frame(width: self.width, height: self.height)
+                    .modifier(NeumorphismShadowModifier(isAnimation: isHeighlight))
+                    .padding()
+
+                
+                Text(self.text)
+                    .font(self.font ?? .title)
+                    .foregroundColor(
+                        isHeighlight ? self.color?.darkerColor() ?? self.neumorphism.color.darkerColor().darkerColor() : self.color ?? self.neumorphism.color.darkerColor())
+            }
+            .animation(Animation.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 1))
+        }
+        .padding()
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct NeumorphismLabelButton_Previews: PreviewProvider {
+    static let neumorphism = NeumorphismManager(
+        lightColor: Color(hex: "C1D2EB"),
+        darkColor: Color(hex: "2C292C")
+    )
+    
+    static var previews: some View {
+        NeumorphismLabelButton()
+            .environmentObject(neumorphism)
+            .previewLayout(.sizeThatFits)
+    }
+}
