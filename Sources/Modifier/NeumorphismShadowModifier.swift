703cb517e6d1cd9feb6fcd4ef850fc2de84bc74e
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-03-30 02:27:54 +0900

    refactor line

--- Sources/Modifier/NeumorphismShadowModifier.swift
+++ Sources/Modifier/NeumorphismShadowModifier.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @available(iOS 13.0, *)
 public struct NeumorphismShadowModifier: ViewModifier {
     @EnvironmentObject var neumorphism: NeumorphismManager
-
+    
     private let darkShadowColor: Color?
     private let lightShadowColor: Color?
     
@@ -11,7 +11,14 @@ public struct NeumorphismShadowModifier: ViewModifier {
     private let x: CGFloat
     private let y: CGFloat
     
-    public init(darkShadowColor: Color? = nil, lightShadowColor: Color? = nil, radius: CGFloat = 8, x: CGFloat = 4, y: CGFloat = 4, isAnimation: Bool = false) {
+    public init(
+        darkShadowColor: Color? = nil,
+        lightShadowColor: Color? = nil,
+        radius: CGFloat = 8,
+        x: CGFloat = 4,
+        y: CGFloat = 4,
+        isAnimation: Bool = false
+    ) {
         self.darkShadowColor = darkShadowColor
         self.lightShadowColor = lightShadowColor
         if isAnimation {
