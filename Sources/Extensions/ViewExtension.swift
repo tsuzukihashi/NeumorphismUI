d897a6081bcd9527bf6e8b75770b38dff2c24c5b
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-03-30 02:30:44 +0900

    fix neumorphismShadow

--- Sources/Extensions/ViewExtension.swift
+++ Sources/Extensions/ViewExtension.swift
@@ -2,7 +2,21 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public extension View {
-    func neumorphismShadow(isAnimation: Bool = false) -> some View {
-        self.modifier(NeumorphismShadowModifier(isAnimation: isAnimation))
+    func neumorphismShadow(
+        darkShadowColor: Color? = nil,
+        lightShadowColor: Color? = nil,
+        radius: CGFloat = 8,
+        x: CGFloat = 4,
+        y: CGFloat = 4,
+        isAnimation: Bool = false
+    ) -> some View {
+        self.modifier(NeumorphismShadowModifier(
+            darkShadowColor: darkShadowColor,
+            lightShadowColor: lightShadowColor,
+            radius: radius,
+            x: x,
+            y: y,
+            isAnimation: isAnimation
+        ))
     }
 }
