83cb5f399ad31293d2ebf6a2bd956ce53eda3c20
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-03-30 02:56:43 +0900

    Added color setting for NeumorphismDentView

--- Sources/CustomViews/NeumorphismDentView.swift
+++ Sources/CustomViews/NeumorphismDentView.swift
@@ -6,29 +6,40 @@ public struct NeumorphismDentView: View {
     
     private let width: CGFloat?
     private let height: CGFloat?
+    private let color: Color?
     private let cornerRadius: CGFloat
 
     public init(
         width: CGFloat? = nil,
         height: CGFloat? = nil,
+        color: Color? = nil,
         cornerRadius: CGFloat = 16
     ) {
         self.width = width
         self.height = height
+        self.color = color
         self.cornerRadius = cornerRadius
     }
     
     public var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: self.cornerRadius)
-                .fill(self.neumorphism.color)
+                .fill(self.color ?? self.neumorphism.color)
                 .frame(width: self.width, height: self.height)
                 .overlay(
                     RoundedRectangle(cornerRadius: self.cornerRadius)
-                        .stroke(self.neumorphism.color, lineWidth: 4)
-                        .shadow(color: self.neumorphism.color.darkerColor(), radius: 4, x: 4, y: 4)
+                        .stroke(self.color ?? self.neumorphism.color, lineWidth: 4)
+                        .shadow(
+                            color: self.color?.darkerColor() ?? self.neumorphism.color.darkerColor(),
+                                radius: 4,
+                                x: 4,
+                                y: 4)
                         .clipShape(RoundedRectangle(cornerRadius: self.cornerRadius))
-                        .shadow(color: self.neumorphism.color.lighterColor(), radius: 2, x: -2, y: -2)
+                        .shadow(
+                            color: self.color?.lighterColor() ?? self.neumorphism.color.lighterColor(),
+                            radius: 2,
+                            x: -2,
+                            y: -2)
                         .clipShape(RoundedRectangle(cornerRadius: self.cornerRadius))
             )
             .cornerRadius(self.cornerRadius)
