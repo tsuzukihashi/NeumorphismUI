cd9d281b9c3d465356b7fb93528929879f433c05
tsuzukihashi <tsudukihashi0817@gmail.com>
2023-04-21 01:07:30 +0900

    fix: format

--- Package.swift
+++ Package.swift
@@ -7,25 +7,23 @@ let package = Package(
     name: "NeumorphismUI",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NeumorphismUI",
-            targets: ["NeumorphismUI"]),
+            targets: ["NeumorphismUI"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "NeumorphismUI",
             dependencies: [],
-            path: "Sources"),
+            path: "Sources"
+        ),
         .testTarget(
             name: "NeumorphismUITests",
             dependencies: ["NeumorphismUI"],
-            path: "Tests"),
+            path: "Tests"
+        ),
     ]
 )
